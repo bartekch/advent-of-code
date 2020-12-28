fafe1b47ef0e1e6e8bb1097943943010b88933c4
Bartosz Chroł <bartek.chrol@gmail.com>
2020-12-28 14:58:41 +0100

    2020 Day 23 Rcpp optimization

--- 2020/day23.R
+++ 2020/day23.R
@@ -4,31 +4,31 @@
 playCups <- function(cups, N = 100) {
   l <- length(cups)
   current_cup_position <- 1
-  
+
   for (i in 1:N) {
     current_cup_value <- cups[current_cup_position]
-    
+
     # pick up three cups
     if (current_cup_position <= l - 3) {
       picked_cups <- cups[current_cup_position + 1:3]
       cups <- c(head(cups, current_cup_position), tail(cups, l - current_cup_position - 3))
-      
+
     } else if (current_cup_position == l - 2) {
       picked_cups <- cups[c(current_cup_position + 1:2, 1)]
       cups <- cups[2:current_cup_position]
       current_cup_position <- current_cup_position - 1
-      
+
     } else if (current_cup_position == l - 1) {
       picked_cups <- cups[c(current_cup_position + 1, 1:2)]
       cups <- cups[3:current_cup_position]
       current_cup_position <- current_cup_position - 2
-      
+
     } else {
       picked_cups <- head(cups, 3)
       cups <- tail(cups, -3)
       current_cup_position <- current_cup_position - 3
     }
-    
+
     # find destination cup
     tmp_ind <- cups < current_cup_value
     destination_cup_position <- which.max(cups[tmp_ind])
@@ -40,21 +40,21 @@ playCups <- function(cups, N = 100) {
     if (destination_cup_position < current_cup_position) {
       current_cup_position <- current_cup_position + 3
     }
-    
+
     # insert picked cups
     cups <- c(cups[1:destination_cup_position], picked_cups, tail(cups, -destination_cup_position))
-    
+
     # find the position of the new current cup
     current_cup_position <- current_cup_position + 1
     if (current_cup_position > l) {
       current_cup_position <- 1
     }
   }
-  
+
   # set up 1 as the first cup
   ind <- which(cups == 1)
   cups <- c(1, tail(cups, -ind), head(cups, ind - 1))
-  
+
   #
   cups
 }
@@ -85,11 +85,11 @@ system.time(
   res_ex2 <- playCups(example2, N = 1e3)
 )
 # N = 1e3
-#   user  system elapsed 
-# 24.634   0.961  25.913 
+#   user  system elapsed
+# 24.634   0.961  25.913
 # N = 1e4
-# user  system elapsed 
-# 261.847   9.642 276.976 
+# user  system elapsed
+# 261.847   9.642 276.976
 # we need N = 1e7, so it looks infeasible in that way
 
 
@@ -124,10 +124,10 @@ system.time(
   res_ex2_cpp <- playCupsCpp(example2, maxit = 1e4)
 )
 # maxit = 1e3
-#   user  system elapsed 
-# 11.290   0.323  11.755 
+#   user  system elapsed
+# 11.290   0.323  11.755
 # maxit = 1e4
-#    user  system elapsed 
+#    user  system elapsed
 # 145.610  13.119 161.223
 
 # still way too slow, need to write better Rcpp
@@ -163,15 +163,15 @@ system.time(
 )
 all.equal(res_ex2_cpp, res_ex2_cpp2)
 # maxit = 1e3
-#  user  system elapsed 
+#  user  system elapsed
 # 0.824   0.004   0.840
 # maxit = 1e4
-#  user  system elapsed 
-# 8.111   0.027   8.239 
+#  user  system elapsed
+# 8.111   0.027   8.239
 all.equal(res_ex2_cpp, res_ex2_cpp2_e4)
 # maxit = 1e5
-#   user  system elapsed 
-# 72.353   0.169  73.526 
+#   user  system elapsed
+# 72.353   0.169  73.526
 
 # so for 1e7 it should take around 7000s ~ 2h
 # way too long, but currently I don't have better ideas so it's worth a shot
@@ -181,8 +181,8 @@ input2 <- c(input, seq(from = max(input) + 1, by = 1L, length.out = 1e6 - length
 system.time(
   res_run2_cpp2 <- playCupsCpp2(input2, maxit = 1e7)
 )
-#     user   system  elapsed 
-# 5791.742   12.304 5902.604 
+#     user   system  elapsed
+# 5791.742   12.304 5902.604
 ind <- which(res_run2_cpp2[[1]] == 1)
 prod(res_run2_cpp2[[1]][ind + 1:2])
 # 11591415792
@@ -210,24 +210,24 @@ recreateList <- function(x, start = 1) {
 # this assumes that there are no gaps
 playCupsOpt <- function(cups, N = 100) {
   current_cup <- cups[1]
-  
+
   # store index of the next cup under each index
   next_cups <- c(tail(cups, -1), cups[1])
   cups <- next_cups[order(cups)]
-  
+
   max_cup <- length(cups)
   picked_cups <- integer(3)
-  
+
   for (i in 1:N) {
-    
+
     # pick up three cups
     picked_cups[1] <- cups[current_cup]
     picked_cups[2] <- cups[picked_cups[1]]
     picked_cups[3] <- cups[picked_cups[2]]
-    
+
     # remove picked cups
     cups[current_cup] <- cups[picked_cups[3]]
-    
+
     # find destination cup
     destination_cup <- current_cup - 1
     while (TRUE) {
@@ -239,19 +239,19 @@ playCupsOpt <- function(cups, N = 100) {
         break
       }
     }
-    
+
     # insert picked cups
     old_next <- cups[destination_cup]
     cups[destination_cup] <- picked_cups[1]
     cups[picked_cups[1]] <- picked_cups[2]
     cups[picked_cups[2]] <- picked_cups[3]
     cups[picked_cups[3]] <- old_next
-    
+
     # find the position of the new current cup
     current_cup <- cups[current_cup]
   }
-  
-  
+
+
   # recreate cups order starting from 1
   recreateList(cups)
 }
@@ -285,8 +285,8 @@ ggplot2::autoplot(bench)
 system.time(
   res_ex2_opt <- playCupsOpt(example2, N = 1e7)
 )
-#   user  system elapsed 
-# 21.656   0.047  21.949 
+#   user  system elapsed
+# 21.656   0.047  21.949
 prod(res_ex2_opt[2:3])
 
 
@@ -295,14 +295,41 @@ prod(res_ex2_opt[2:3])
 system.time(
   res_run2_opt <- playCupsOpt(input2, N = 1e7)
 )
-#   user  system elapsed 
-# 20.810   0.056  21.107 
+#   user  system elapsed
+# 20.810   0.056  21.107
 # MUCH better (100x time than Rcpp solution)
 prod(res_run2_opt[2:3])
 
 
 
-# now we could recreate it in Rcpp as well - should be even faster
 
-## Rcpp implementation after optimization
+### Rcpp implementation after optimization
 
+system.time(
+  res_ex2_cpp_opt <- playCupsCppOpt(example2, maxit = 1e7)
+)
+# user  system elapsed
+# 0.362   0.000   0.364
+prod(res_ex2_cpp_opt[2:3])
+all.equal(res_ex2_opt, res_ex2_cpp_opt)
+
+
+system.time(
+  res_run2_cpp_opt <- playCupsCppOpt(input2, maxit = 1e7)
+)
+# user  system elapsed
+# 0.362   0.000   0.364
+prod(res_run2_cpp_opt[2:3])
+all.equal(res_run2_opt, res_run2_cpp_opt)
+
+
+
+# speed comparison
+bench <- microbenchmark::microbenchmark(
+  playCupsOpt(input2, N = 1e7),
+  playCupsCppOpt(input2, maxit = 1e7),
+  times = 10
+)
+bench
+ggplot2::autoplot(bench)
+# R destroyed
--- 2020/day23.cpp
+++ 2020/day23.cpp
@@ -13,49 +13,49 @@ int which(IntegerVector x, int v) {
 //[[Rcpp::export]]
 IntegerVector playCupsCpp(IntegerVector cups, int maxit = 100) {
   int l = cups.length() - 1;
-  
+
   int current_cup_position = 0;
-  
+
   IntegerVector picked_cups;
   int current_cup_value, destination_cup_value, destination_cup_position;
-  
-  
+
+
   for (int i = 1; i <= maxit; i++) {
-    
+
     current_cup_value = cups[current_cup_position];
-    
+
     //  pick up three cups
     if (current_cup_position <= l - 3) {
       picked_cups = cups[Range(current_cup_position + 1, current_cup_position + 3)];
-      
+
       cups.erase(current_cup_position + 1, current_cup_position + 4);
-      
+
     } else if (current_cup_position == l - 2) {
       picked_cups = cups[Range(current_cup_position + 1, current_cup_position + 2)];
       picked_cups.push_back(cups[0]);
-      
+
       cups.erase(current_cup_position + 1, current_cup_position + 3);
       cups.erase(0);
-      
+
       current_cup_position--;
-      
+
     } else if (current_cup_position == l - 1) {
       picked_cups = cups[Range(current_cup_position + 1, current_cup_position + 1)];
       picked_cups.push_back(cups[0]);
       picked_cups.push_back(cups[1]);
-      
+
       cups.erase(current_cup_position + 1, current_cup_position + 2);
       cups.erase(0, 2);
-      
+
       current_cup_position -= 2;
-        
+
     } else {
       picked_cups = head(cups, 3);
       cups.erase(0, 3);
-      
+
       current_cup_position -= 3;
     }
-    
+
     // find destination cup
     destination_cup_value = current_cup_value - 1;
     while (true) {
@@ -71,21 +71,21 @@ IntegerVector playCupsCpp(IntegerVector cups, int maxit = 100) {
     if (destination_cup_position < current_cup_position) {
       current_cup_position += 3;
     }
-    
+
     // insert picked cups
     for (int j = 1; j <= 3; j++) {
       cups.insert(destination_cup_position + j, picked_cups[j-1]);
     }
-    
+
     // find the position of the new current cup
     current_cup_position++;
     if (current_cup_position > l) {
           current_cup_position = 0;
     }
   }
-  
+
   // shift to set 1 as the first cup
-  
+
   return cups;
 }
 
@@ -109,58 +109,58 @@ int which2(std::vector<int> x, int v) {
 //[[Rcpp::export]]
 List playCupsCpp2(IntegerVector cupsR, int maxit = 100) {
   std::vector<int>  cups = as<std::vector<int>>(cupsR);
-  
+
   int l = cups.size() - 1;
-  
+
   std::vector<int>::iterator current_cup_position = cups.begin();
-  
+
   std::vector<int> picked_cups = {0,0,0};
   int current_cup_value, destination_cup_value;
   std::vector<int>::iterator destination_cup_position;
-  
+
   for (int i = 1; i <= maxit; i++) {
-    
+
     current_cup_value = *current_cup_position;
-    
+
     //  pick up three cups
     if (current_cup_position <= cups.end() - 4) {
       picked_cups[0] = *(current_cup_position + 1);
       picked_cups[1] = *(current_cup_position + 2);
       picked_cups[2] = *(current_cup_position + 3);
-      
+
       cups.erase(current_cup_position + 1, current_cup_position + 4);
-      
+
     } else if (current_cup_position == cups.end() - 3) {
       picked_cups[0] = *(current_cup_position + 1);
       picked_cups[1] = *(current_cup_position + 2);
       picked_cups[2] = *cups.begin();
-      
-      
+
+
       cups.erase(current_cup_position + 1, current_cup_position + 3);
       cups.erase(cups.begin());
-      
+
       current_cup_position--;
-      
+
     } else if (current_cup_position == cups.end() - 2) {
       picked_cups[0] = *(current_cup_position + 1);
       picked_cups[1] = *cups.begin();
       picked_cups[2] = *(cups.begin() + 1);
-      
+
       cups.erase(current_cup_position + 1, current_cup_position + 2);
       cups.erase(cups.begin(), cups.begin() + 2);
-      
+
       current_cup_position--;current_cup_position--;
-      
+
     } else {
       picked_cups[0] = *cups.begin();
       picked_cups[1] = *(cups.begin() + 1);
       picked_cups[2] = *(cups.begin() + 2);
-      
+
       cups.erase(cups.begin(), cups.begin() + 3);
-      
+
       current_cup_position--;current_cup_position--;current_cup_position--;
     }
-    
+
     // find destination cup
     destination_cup_value = current_cup_value - 1;
     while (true) {
@@ -174,7 +174,7 @@ List playCupsCpp2(IntegerVector cupsR, int maxit = 100) {
     }
     destination_cup_position = std::find(cups.begin(), cups.end(), destination_cup_value);
     // insert picked cups
-    
+
     //std::cout << "round" << std::endl;
     //std::cout << i << std::endl;
     //std::cout << *current_cup_position << std::endl;
@@ -184,7 +184,7 @@ List playCupsCpp2(IntegerVector cupsR, int maxit = 100) {
       current_cup_position++;current_cup_position++;current_cup_position++;
     }
     //std::cout << *current_cup_position << std::endl;
-    
+
     // find the position of the new current cup
     if (current_cup_position == (cups.end() - 1)) {
       current_cup_position = cups.begin();
@@ -193,8 +193,75 @@ List playCupsCpp2(IntegerVector cupsR, int maxit = 100) {
     }
     //std::cout << *current_cup_position << std::endl;
   }
-  
+
   // shift to set 1 as the first cup
   List L = List::create(wrap(cups), *current_cup_position);
   return L;
+}
+
+
+
+
+
+
+//[[Rcpp::export]]
+IntegerVector playCupsCppOpt(IntegerVector cupsR, int maxit = 100) {
+  int current_cup = cupsR[0] - 1;
+
+  int max_cup = cupsR.size() - 1;
+
+  // store index of the next cup under each index
+  IntegerVector cups (max_cup + 1);
+  for (int i = 0; i < max_cup; i++) {
+    cups[cupsR[i] - 1] = cupsR[i + 1] - 1;
+  }
+  cups[cupsR[max_cup] - 1] = current_cup;
+
+
+  IntegerVector picked_cups = {0,0,0};
+
+  int destination_cup;
+
+  for (int i = 1; i <= maxit; i++) {
+
+    // pick up three cups
+    picked_cups[0] = cups[current_cup];
+    picked_cups[1] = cups[picked_cups[0]];
+    picked_cups[2] = cups[picked_cups[1]];
+
+
+    // remove picked cups
+    cups[current_cup] = cups[picked_cups[2]];
+
+
+    // find destination cup
+    destination_cup = current_cup - 1;
+    while (true) {
+      if (destination_cup < 0) {
+        destination_cup = max_cup;
+      } else if (destination_cup == picked_cups[0] || destination_cup == picked_cups[1] || destination_cup == picked_cups[2]) {
+        destination_cup--;
+      } else {
+        break;
+      }
+    }
+
+    // insert picked cups
+    cups[picked_cups[2]] = cups[destination_cup];
+    cups[picked_cups[1]] = picked_cups[2];
+    cups[picked_cups[0]] = picked_cups[1];
+    cups[destination_cup] = picked_cups[0];
+
+    // find the position of the new current cup
+    current_cup = cups[current_cup];
+  }
+
+  // recreateList starting from 0
+  IntegerVector res (max_cup + 1);
+  res[0] = 1;
+  for (int i = 0; i < max_cup; i++) {
+    res[i + 1] = cups[res[i] - 1] + 1;
+  }
+
+  return res;
 }
